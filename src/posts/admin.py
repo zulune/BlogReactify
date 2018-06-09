94caadd0d692d3bc900323cdcaa5821e07b9ab0c
zulune <mars.xaker@gmail.com>
2018-06-10 01:43:11 +0300

    Create model Post

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Post
 # Register your models here.
+
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'updated', 'timestamp', )
+    list_display_links = ('updated', )
+    list_editable = ('title', )
+    list_filter = ('updated', 'timestamp', )
+
+    search_fields = ('title', 'content', )
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save
 
+from .utils import unique_slug_generator
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, null=True, unique=True)
+    content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField()
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-timestamp', '-updated')
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
