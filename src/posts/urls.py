ab3811f89f01d56fdb764b5c2f08114dbc4c1f08
zulune <mars.xaker@gmail.com>
2018-06-10 15:24:17 +0300

    Post serializer and view

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import *
+from django.urls import path, re_path
+from .views import (
+    PostDetailAPIView,
+    PostListCreateAPIView
+)
 
+app_name = 'post-api'
 
 urlpatterns = [
-
+    path('', PostListCreateAPIView.as_view(), name='list-create'),
+    re_path(r'^(?P<slug>[\w-]+)/$', PostDetailAPIView.as_view(), name='detail'),
 ]
