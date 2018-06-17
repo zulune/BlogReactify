067df034509316cb4354d677f0118ec28cb5fb19
zulune <mars.xaker@gmail.com>
2018-06-17 16:03:56 +0300

    Post detail view

--- src/react-ui/src/Posts/PosrDetail.js
+++ src/react-ui/src/Posts/PosrDetail.js
@@ -5,29 +5,72 @@ class PostDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            slug: null
+            slug: null,
+            post: null,
+            doneLoading: false,
         }
+    }
+
+    loadPost(slug) {
+        const endpoint = `/api/posts/${slug}`;
+        let thisComp = this;
+        let lookupOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        fetch(endpoint, lookupOptions)
+        .then(response => {
+            if (response.status == 404) {
+                console.log('404! Page not found!');
+            }
+            return response.json()
+        }).then(responseData => {
+            if (responseData.detail) {
+                thisComp.setState({
+                    doneLoading: true,
+                    post: null
+                })
+            } else {
+                thisComp.setState({
+                    doneLoading: true,
+                    post: responseData
+                })
+            }
+        }).catch(error => {
+            console.log(error);
+        })
     }
     
     componentDidMount() {
         this.setState({
-            slug: null
+            slug: null,
+            post: null
         })
         if (this.props.match) {
             const {slug} = this.props.match.params;
             this.setState({
-                slug: slug
+                slug: slug,
+                doneLoading: false
             })
+            this.loadPost(slug)
         }
     }
     render() {
-        const {slug} = this.state;
+        const {doneLoading} = this.state;
+        const {post} = this.state;
         return (
             <div className="col">
                 <p>{
-                    (slug !== null) 
+                    (doneLoading === true) 
                         ? <div className="jumbotron">
-                            {slug}
+                            {post === null ? "Not found!":
+                            <div>
+                            <h1 className="display-4">{post.title}</h1>
+                            {post.slug}
+                            <p className="lead">{post.content}</p>
+                            <hr className="my-4" />
                             <p className='lead'>
                                 <Link maintainScrollPosition={false} to={{
                                     pathname: `/posts`,
@@ -35,9 +78,10 @@ class PostDetail extends Component {
                                 }}>
                                 Posts
                                 </Link>
-                            </p>
+                            </p> 
+                        </div>}
                         </div> 
-                        : <div className="alert alert-info">Not found</div>
+                        : <div className="alert alert-info">Loading...</div>
                     }
                 </p>
             </div>
